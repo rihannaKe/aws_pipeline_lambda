4e2e86fdd00c8affb0ef3bef1cfce13b55e5d248
IBRAHIM Rihanna <rihanna.ibrahim@wfp.org>
2023-07-23 15:30:14 +0200

    build

---
+++ lambda_function.py
@@ -0,0 +1,18 @@
+import json
+import requests
+from collections import defaultdict
+from datetime import datetime
+
+
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps('Hello World!!'
+        )
+    }
