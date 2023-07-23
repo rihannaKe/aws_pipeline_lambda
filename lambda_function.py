850790bfce9ecd1a0271b85ae5d8f5eaae905e9b
IBRAHIM Rihanna <rihanna.ibrahim@wfp.org>
2023-07-23 18:09:06 +0200

    last change

--- lambda_function.py
+++ lambda_function.py
@@ -4,12 +4,7 @@ from collections import defaultdict
 from datetime import datetime
 
 
-def get_data(iso3, start_date, end_date):
-    url = f'https://api.hungermapdata.org/v1/foodsecurity/country/{iso3}/region?date_start={start_date}&date_end={end_date}'
-    response = requests.get(url)
-    response_json = response.json()
-    return response_json
-
+''' Utility function to transfrom the array data into object with defined properties '''
 def organize_by_admin1(raw_arr):
     out = {}
     for d in raw_arr:
@@ -18,12 +13,31 @@ def organize_by_admin1(raw_arr):
                                                       'metrics': d["metrics"]})
     return out
 
+''' Utility function to transfrom the array data into objects with keys given by key name '''
 def organize_by_date(raw_arr, key_name):
     output = defaultdict(lambda: [])
     for item in raw_arr:
         output[item[key_name]].append(item)
     return output
 
+
+''' 
+    Function used to fetch data from network. If there is issue then it will fetch data from the another source (local json file)
+    Not ideal for producation. A fallback to data id not aviable must be implemneted diffrently, but at least this will 
+    avoid relying only in the api response for the endpoint to work
+'''
+def get_data(iso3, start_date, end_date):
+    try: 
+        url = f'https://api.hungermapdata.org/v1/foodsecurity/country/{iso3}/region?date_start={start_date}&date_end={end_date}'
+        response = requests.get(url)
+        response_json = response.json()
+        return response_json, True
+    except:
+        with open('last_calculated_data.json', 'r') as f:
+            data = json.load(f)
+            return data[iso3], False
+
+''' Auxillary function used to calculate the average given an array'''
 def calculate_average(raw_arr, key_name):
     def str_to_date(str_date):
         date_obj = datetime.strptime(str_date, '%Y-%m-%d')
@@ -42,6 +56,7 @@ def calculate_average(raw_arr, key_name):
 
     return average
 
+''' Auxillary function used to calculate the monthly average (1st requirment of Task2)'''
 def calculate_adm1_monthly_average(raw_data):
     adm1_data = organize_by_admin1(raw_data)
     out = []
@@ -52,6 +67,7 @@ def calculate_adm1_monthly_average(raw_data):
         })
     return out
 
+''' Function used to calculate the national esitmate (2nd requirment of Task2)'''
 def calculate_national_estimate(raw_data):
     total_people = 0
     weighted_prevalence_sum = 0
@@ -68,6 +84,7 @@ def calculate_national_estimate(raw_data):
 
     return total_people, national_prevalence_estimate
 
+''' Function used to calculate the national esitmate (3rd requirment of Task2)'''
 def calculate_national_estimate_variance(raw_data, national_prevalence_estimate):
     num_regions = len(raw_data)
     squared_diff_sum = 0
@@ -81,6 +98,7 @@ def calculate_national_estimate_variance(raw_data, national_prevalence_estimate)
 
     return variance
 
+''' Function used to respond with the national esitmate and variance(2nd and 3rd requirment of Task2)'''
 def calculate_daily_national_metrics(raw_data):
     date_key = organize_by_date(raw_data, 'date')
     data = {}
@@ -94,29 +112,22 @@ def calculate_daily_national_metrics(raw_data):
         }
     return data
 
-def calculate_metrics(iso3, start_date, end_date):
-    try:
-        raw_data = get_data(iso3, start_date, end_date)
+''' Aggregator function'''
+def get_iso3_metrics(iso3, start_date, end_date):
+    raw_data, fresh = get_data(iso3, start_date, end_date)
+    if fresh:
         metric_a = calculate_adm1_monthly_average(raw_data)
         metric_b = calculate_daily_national_metrics(raw_data)
-    except:
-        metric_a , metric_b = None, None
-    finally:
-        return { 'metric_a': metric_a, 'metric_b': metric_b}
+    else:
+        metric_a = raw_data['metric_a']
+        metric_b = raw_data['metric_b']
+    return { 'metric_a': metric_a, 'metric_b': metric_b}
 
-def get_metrics_data(iso3, start_date, end_date):
-    try:
-        # First, try to call the `calculate_metrics()` function to calculate the metrics data.
-        return calculate_metrics(iso3, start_date, end_date)
-    except:
-        with open('last_calculated_data.json', 'r') as f:
-            data = json.load(f)
-            return data[iso3]
-
+''' aws lambda handler function'''
 def lambda_handler(event, context):
     start_date, end_date = '2022-06-01', '2023-06-01'
-    COL_data = get_metrics_data('COL', start_date, end_date)
-    BFA_data = get_metrics_data('BFA', start_date, end_date)
+    COL_data = get_iso3_metrics('COL', start_date, end_date)
+    BFA_data = get_iso3_metrics('BFA', start_date, end_date)
     return {
         'statusCode': 200,
         'headers': {
